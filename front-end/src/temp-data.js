c2423b845ac8f8a92a4032f5191d059135ffc447
TontonBanton <pancla13ulas@gmail.com>
2024-09-20 10:42:42 +0800

    Image Assets Temp-data

---
+++ front-end/src/temp-data.js
@@ -0,0 +1,45 @@
+import product1 from './assets/product1.jpg'
+import product2 from './assets/product2.jpg'
+import product3 from './assets/product3.jpg'
+import product4 from './assets/product4.jpg'
+import product5 from './assets/product5.jpg'
+import product6 from './assets/product6.jpg'
+
+export const products = [
+  {
+    id: '001',
+    name: 'product1',
+    price: '13.00',
+    image: product1
+  },
+  {
+    id: '002',
+    name: 'product2',
+    price: '15.00',
+    image: product2
+  },
+  {
+    id: '003',
+    name: 'product3',
+    price: '18.00',
+    image: product3
+  },
+  {
+    id: '004',
+    name: 'product4',
+    price: '12.50',
+    image: product4
+  },
+  {
+    id: '005',
+    name: 'product5',
+    price: '20.00',
+    image: product5
+  },
+  {
+    id: '006',
+    name: 'product6',
+    price: '10.99',
+    image: product6
+  }
+]
